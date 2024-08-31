819f2fe725708d7639b770da9dcfb69175557a3b
Henrijs Obolevičs <bonis.obo@gmail.com>
2024-08-31 17:31:18 +0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Henrijs Obolevics on 31/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
