2f1c772f6f8bd1c58183098ec56c61eb85234d20
Henrijs Obolevičs <bonis.obo@gmail.com>
2024-10-27 20:08:55 +0200

    Added camera functionality for adding categories

--- Keitas Skapis/PievienotApgerbu.swift
+++ Keitas Skapis/PievienotApgerbu.swift
@@ -13,13 +13,16 @@ struct PievienotApgerbuView: View {
     @Query private var kategorijas: [Kategorija]
     @Query private var apgerbi: [Apgerbs]
     
+    @Environment(\.dismiss) var dismiss
     @Environment(\.modelContext) private var modelContext
     
     @State var apgerbaNosaukums = ""
     @State var apgerbaPiezimes = ""
     
     
-    @State private var isExpanded = false
+    @State private var isExpandedKategorijas = false
+    @State private var isExpandedSezona = false
+
     @State var apgerbaKategorijas: Set<Kategorija> = []
     
     @State var izveletaKrasa: Color = .white
@@ -44,157 +47,161 @@ struct PievienotApgerbuView: View {
     
     
     var body: some View {
-        HStack {
-            Text("Pievienot Apģērbu").font(.title).bold()
+        VStack {
+            HStack {
+                Text("Pievienot Apģērbu").font(.title).bold()
+                Spacer()
+                Button(action: {dismiss()}) {
+                    Image(systemName: "arrowshape.left.fill").font(.title).foregroundStyle(.black)
+                }.navigationBarBackButtonHidden(true)
+            }.padding()
             Spacer()
-            Button(action: atpakal) {
-                Image(systemName: "arrowshape.left.fill").font(.title).foregroundStyle(.black)
-            }
-        }.padding()
-        Spacer()
-        ScrollView {
-            VStack (alignment: .leading) {
-                
-                Button (action: pievienotFoto) {
-                    ZStack {
-                        Image(systemName: "rectangle.portrait.fill").resizable().frame(width: 60, height: 90).foregroundStyle(.gray).opacity(0.20)
-                        Image(systemName: "camera").foregroundStyle(.black).font(.title2)
+            ScrollView {
+                VStack (alignment: .leading) {
+                    
+                    Button (action: pievienotFoto) {
+                        ZStack {
+                            Image(systemName: "rectangle.portrait.fill").resizable().frame(width: 60, height: 90).foregroundStyle(.gray).opacity(0.20)
+                            Image(systemName: "camera").foregroundStyle(.black).font(.title2)
+                        }
                     }
+                    
+                    TextField("Nosaukums", text: $apgerbaNosaukums).textFieldStyle(.roundedBorder).padding(.top, 20)
+                    TextField("Piezīmes", text: $apgerbaPiezimes).textFieldStyle(.roundedBorder).padding(.top, 10)
+                    
+                    
+                    VStack(alignment: .leading) {
+                        Button(action: { isExpandedKategorijas.toggle() }) {
+                            HStack {
+                                Text("Kategorijas").foregroundStyle(.black)
+                                Spacer()
+                                Image(systemName: isExpandedKategorijas ? "chevron.up" : "chevron.down").foregroundStyle(.black)
+                            }
+                            .padding(10)
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(8)
+                        }
+                        
+                        if isExpandedKategorijas {
+                            VStack {
+                                ForEach(kategorijas, id: \.self) { kategorija in
+                                    HStack {
+                                        Text(kategorija.nosaukums)
+                                        Spacer()
+                                        if apgerbaKategorijas.contains(kategorija) {
+                                            Image(systemName: "checkmark")
+                                        }
+                                    }
+                                    .padding()
+                                    .onTapGesture {
+                                        if apgerbaKategorijas.contains(kategorija) {
+                                            apgerbaKategorijas.remove(kategorija)
+                                        } else {
+                                            apgerbaKategorijas.insert(kategorija)
+                                        }
+                                    }
+                                }
+                            }
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .shadow(radius: 5)
+                        }
+                    }
+                    .padding(.top, 10)
+                    
+                    ColorPicker("Krāsa", selection: Binding(
+                        get: { izveletaKrasa },
+                        set: { jaunaKrasa in
+                            izveletaKrasa = jaunaKrasa
+                            apgerbaKrasa = Krasa(color: jaunaKrasa)
+                        })).padding(8)
+                    
+                    Picker("Izmērs", selection: $apgerbaIzmers) {
+                        Text("XS").tag(0)
+                        Text("S").tag(1)
+                        Text("M").tag(3)
+                        Text("L").tag(4)
+                        Text("XL").tag(5)
+                    }.pickerStyle(.segmented).padding(.top, 10)
+                    
+                    Picker("Stāvoklis", selection: $apgerbaStavoklis) {
+                        Text("Tīrs").tag(0)
+                        Text("Netīrs").tag(1)
+                        Text("Mazgājas").tag(2)
+                    }.pickerStyle(.segmented).padding(.top, 10)
+                    
+                    //                let columns = [GridItem(.flexible()), GridItem(.flexible())]
+                    //                LazyVGrid(columns: columns, spacing: 15) {
+                    //                     ForEach(Sezona.allCases, id: \.self) { sezona in
+                    //                         Toggle(sezona.rawValue, isOn: Binding(
+                    //                            get: { apgerbaSezona.contains(sezona) },
+                    //                             set: { izvelets in
+                    //                                 if izvelets {
+                    //                                     apgerbaSezona.insert(sezona)
+                    //                                 } else {
+                    //                                     apgerbaSezona.remove(sezona)
+                    //                                 }
+                    //                             }
+                    //                         ))
+                    //                     }
+                    //                 }
+                    //                .padding(.top, 15).padding(.horizontal, 5)
+                    
+                    VStack(alignment: .leading) {
+                        Button(action: { isExpandedSezona.toggle() }) {
+                            HStack {
+                                Text("Sezona").foregroundStyle(.black)
+                                Spacer()
+                                Image(systemName: isExpandedSezona ? "chevron.up" : "chevron.down").foregroundStyle(.black)
+                            }
+                            .padding(10)
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(8)
+                        }
+                        
+                        if isExpandedSezona {
+                            VStack {
+                                ForEach(sezonaIzvele, id: \.self) { sezona in
+                                    HStack {
+                                        Text(sezona.rawValue)
+                                        Spacer()
+                                        if apgerbaSezona.contains(sezona) {
+                                            Image(systemName: "checkmark")
+                                        }
+                                    }
+                                    .padding()
+                                    .onTapGesture {
+                                        if apgerbaSezona.contains(sezona) {
+                                            apgerbaSezona.remove(sezona)
+                                        } else {
+                                            apgerbaSezona.insert(sezona)
+                                        }
+                                    }
+                                }
+                            }
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .shadow(radius: 5)
+                        }
+                    }
+                    .padding(.top, 10)
+                    
+                    
+                    DatePicker("Pēdējoreiz vilkts", selection: $apgerbsPedejoreizVilkts, displayedComponents: [.date]).padding(.top, 15).padding(.horizontal, 5)
+                    
+                    Toggle(isOn: $apgerbsGludinams) {
+                        Text("Gludināms")
+                    }.padding(.top, 15).padding(.horizontal, 5)
+                    
                 }
-                
-                TextField("Nosaukums", text: $apgerbaNosaukums).textFieldStyle(.roundedBorder).padding(.top, 20)
-                TextField("Piezīmes", text: $apgerbaPiezimes).textFieldStyle(.roundedBorder).padding(.top, 10)
-                
-  
-                VStack(alignment: .leading) {
-                  Button(action: { isExpanded.toggle() }) {
-                    HStack {
-                        Text("Kategorijas").foregroundStyle(.black)
-                      Spacer()
-                        Image(systemName: isExpanded ? "chevron.up" : "chevron.down").foregroundStyle(.black)
-                    }
-                    .padding(10)
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                  }
-
-                  if isExpanded {
-                    VStack {
-                      ForEach(kategorijas, id: \.self) { kategorija in
-                        HStack {
-                            Text(kategorija.nosaukums)
-                          Spacer()
-                            if apgerbaKategorijas.contains(kategorija) {
-                            Image(systemName: "checkmark")
-                          }
-                        }
-                        .padding()
-                        .onTapGesture {
-                            if apgerbaKategorijas.contains(kategorija) {
-                                apgerbaKategorijas.remove(kategorija)
-                          } else {
-                              apgerbaKategorijas.insert(kategorija)
-                          }
-                        }
-                      }
-                    }
-                    .background(Color.white)
-                    .cornerRadius(8)
-                    .shadow(radius: 5)
-                  }
-                }
-                .padding(.top, 10)
-                
-               ColorPicker("Krāsa", selection: Binding(
-                    get: { izveletaKrasa },
-                    set: { jaunaKrasa in
-                        izveletaKrasa = jaunaKrasa
-                        apgerbaKrasa = Krasa(color: jaunaKrasa)
-                    })).padding(8)
-                      
-                Picker("Izmērs", selection: $apgerbaIzmers) {
-                    Text("XS").tag(0)
-                    Text("S").tag(1)
-                    Text("M").tag(3)
-                    Text("L").tag(4)
-                    Text("XL").tag(5)
-                }.pickerStyle(.segmented).padding(.top, 10)
-                
-                Picker("Stāvoklis", selection: $apgerbaStavoklis) {
-                    Text("Tīrs").tag(0)
-                    Text("Netīrs").tag(1)
-                    Text("Mazgājas").tag(2)
-                }.pickerStyle(.segmented).padding(.top, 10)
-                
-//                let columns = [GridItem(.flexible()), GridItem(.flexible())]
-//                LazyVGrid(columns: columns, spacing: 15) {
-//                     ForEach(Sezona.allCases, id: \.self) { sezona in
-//                         Toggle(sezona.rawValue, isOn: Binding(
-//                            get: { apgerbaSezona.contains(sezona) },
-//                             set: { izvelets in
-//                                 if izvelets {
-//                                     apgerbaSezona.insert(sezona)
-//                                 } else {
-//                                     apgerbaSezona.remove(sezona)
-//                                 }
-//                             }
-//                         ))
-//                     }
-//                 }
-//                .padding(.top, 15).padding(.horizontal, 5)
-                
-                VStack(alignment: .leading) {
-                  Button(action: { isExpanded.toggle() }) {
-                    HStack {
-                        Text("Sezona").foregroundStyle(.black)
-                      Spacer()
-                        Image(systemName: isExpanded ? "chevron.up" : "chevron.down").foregroundStyle(.black)
-                    }
-                    .padding(10)
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                  }
-
-                  if isExpanded {
-                    VStack {
-                        ForEach(sezonaIzvele, id: \.self) { sezona in
-                        HStack {
-                            Text(sezona.rawValue)
-                          Spacer()
-                            if apgerbaSezona.contains(sezona) {
-                            Image(systemName: "checkmark")
-                          }
-                        }
-                        .padding()
-                        .onTapGesture {
-                            if apgerbaSezona.contains(sezona) {
-                                apgerbaSezona.remove(sezona)
-                          } else {
-                              apgerbaSezona.insert(sezona)
-                          }
-                        }
-                      }
-                    }
-                    .background(Color.white)
-                    .cornerRadius(8)
-                    .shadow(radius: 5)
-                  }
-                }
-                .padding(.top, 10)
-                
-                
-                DatePicker("Pēdējoreiz vilkts", selection: $apgerbsPedejoreizVilkts, displayedComponents: [.date]).padding(.top, 15).padding(.horizontal, 5)
-                
-                Toggle(isOn: $apgerbsGludinams) {
-                    Text("Gludināms")
-                }.padding(.top, 15).padding(.horizontal, 5)
-
-            }
-        }.padding()
-    }
-    
-    func atpakal () {
-        print("eju atpakal")
+            }.padding()
+        }.preferredColorScheme(.light)
+            .background(Image("wardrobe_background")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .blur(radius: 5)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/))
+                                    
     }
     
     func pievienotFoto () {
--- Keitas Skapis/PievienotKategoriju.swift
+++ Keitas Skapis/PievienotKategoriju.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import UIKit
 
 struct PievienotKategorijuView: View {
     
@@ -15,34 +16,134 @@ struct PievienotKategorijuView: View {
     
     @State var kategorijasNosaukums = ""
     
+    @State private var selectedImage: UIImage?
+    @State private var isPickerPresented = false
+    @State private var sourceType: UIImagePickerController.SourceType?
+    
+    @State private var showingOption = false
+    
+    struct ImagePicker: UIViewControllerRepresentable {
+        @Environment(\.presentationMode) private var presentationMode
+        @Binding var selectedImage: UIImage?
+        var sourceType: UIImagePickerController.SourceType
+
+        func makeUIViewController(context: Context) -> UIImagePickerController {
+            let picker = UIImagePickerController()
+            picker.delegate = context.coordinator
+            picker.sourceType = sourceType
+            return picker
+        }
+
+        func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+        func makeCoordinator() -> Coordinator {
+            Coordinator(self)
+        }
+
+        class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+            let parent: ImagePicker
+
+            init(_ parent: ImagePicker) {
+                self.parent = parent
+            }
+
+            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                if let image = info[.originalImage] as? UIImage {
+                    parent.selectedImage = image
+                }
+                parent.presentationMode.wrappedValue.dismiss()
+            }
+            
+            func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                parent.presentationMode.wrappedValue.dismiss()
+            }
+        }
+    }
+
+    
     var body: some View {
         HStack {
             Text("Pievienot Kategoriju").font(.title).bold()
+            
             Spacer()
+            
             Button(action: {dismiss()}) {
                 Image(systemName: "arrowshape.left.fill").font(.title).foregroundStyle(.black)
             }.navigationBarBackButtonHidden(true)
         }.padding()
+        
         VStack (alignment: .leading) {
             Button (action: pievienotFoto) {
                 ZStack {
                     Image(systemName: "rectangle.portrait.fill").resizable().frame(width: 60, height: 90).foregroundStyle(.gray).opacity(0.50)
                     Image(systemName: "camera").foregroundStyle(.black).font(.title2)
                 }
+            }.confirmationDialog("Change background", isPresented: $showingOption) {
+                Button("Camera") {
+                    sourceType = .camera
+                    isPickerPresented = true
+                }
+                Button("Photo Library") {
+                    sourceType = .photoLibrary
+                    isPickerPresented = true
+                }
+                Button("Cancel", role: .cancel) { }
             }
             TextField("Nosaukums", text: $kategorijasNosaukums).textFieldStyle(.roundedBorder).padding(.top, 20)
         }.padding(.top, 50).padding(.horizontal, 20)
+            .sheet(isPresented: $isPickerPresented) {
+                    if let sourceType = sourceType {
+                        ImagePicker(selectedImage: $selectedImage, sourceType: sourceType)
+                    }
+                }
+        
         Spacer()
+        
         HStack {
             Button (action: apstiprinat) {
                 Text("Apstiprinat")
             }
             Spacer()
         }.padding()
+        
+//        VStack {
+//             // Display the selected image
+//             if let image = selectedImage {
+//                 Image(uiImage: image)
+//                     .resizable()
+//                     .scaledToFit()
+//                     .frame(width: 200, height: 200)
+//             } else {
+//                 Text("Select an Image")
+//                     .font(.headline)
+//             }
+//
+//             // Buttons to pick image or take photo
+//             HStack {
+//                 Button("Pick from Gallery") {
+//                     sourceType = .photoLibrary
+//                     DispatchQueue.main.async {
+//                         isPickerPresented = true
+//                     }
+//                 }
+//
+//                 Button("Take Photo") {
+//                     sourceType = .camera
+//                     DispatchQueue.main.async {
+//                         isPickerPresented = true
+//                     }
+//                 }
+//            }
+//         }
+//        .sheet(isPresented: $isPickerPresented) {
+//            if let sourceType = sourceType {
+//                ImagePicker(selectedImage: $selectedImage, sourceType: sourceType)
+//            }
+//        }
     }
     
     func pievienotFoto () {
-        print("foto pievienosana very cool")
+        showingOption = true
     }
     
     func apstiprinat() {
